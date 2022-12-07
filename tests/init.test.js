471d9cef52e0b589a4d536eb7432f1532c1a8435
dimitrisfot <106768376+dimifot@users.noreply.github.com>
2022-12-07 21:04:41 +0200

    Update init.test.js

--- tests/init.test.js
+++ tests/init.test.js
@@ -23,7 +23,7 @@ test('GET /statistics returns correct response and status code', async (t) => {
   const {body, statusCode} = await t.context.got('general/statistics');
   //t.is(body.sources,1);
   //body,sources=0
-  t.is(body.sources,1);
+  t.is(body.sources,0);
   t.assert(body.success);
   t.is(statusCode, 200);
 });
