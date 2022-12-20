ca58b2806462de60d1c9188b75591205d30eb1dc
Antonis <atzatzidis@gmail.com>
2022-12-20 23:02:07 +0200

    Create a user with a smaller than allowed password test

--- tests/model_test.js
+++ tests/model_test.js
@@ -5,20 +5,23 @@ const test = require('ava').default;
 const User = require('../src/models/user');
 const db_connect = require('../src/config/mongoose.js');
 
-async function userWithoutUsername(){
+async function createUser(username='',password='',email=''){
     try {
-        await new User().save();
+        await new User({username,password,email}).save();
         return 0;
     }
     catch(e){
-        //console.log('%s',e);
         return Promise.reject(reason=e);
     }
 }
-test('Create user without username',async t => {
+test('Create user without username/password/email',async t => {
     var conn = await db_connect;
-    const result =  await t.throwsAsync(userWithoutUsername());
-    t.is(result.message,'users validation failed: password: User password is required, username: Username is required, email: User email is required')
-    
-  });
+    const result =  await t.throwsAsync(createUser());
+    t.is(result.message,'users validation failed: username: Username is required, password: User password is required, email: User email is required')
+});
 
+test('Create user with password less than minimum length',async t => {
+    var conn = await db_connect;
+    const result2 =  await t.throwsAsync(createUser('bot','beep','notabot@gmail.com'));
+    t.is(result2.message,'users validation failed: password: Path `password` (`beep`) is shorter than the minimum allowed length (5).')
+});
