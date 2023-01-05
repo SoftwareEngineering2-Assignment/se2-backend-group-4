f35e8ca7637e2a70f38f4d7d895f96b5271d5c68
dimitrisfot <foteinogloudimitris@gmail.com>
2023-01-05 16:11:47 +0200

    decrease number of parameters

--- tests/user_unit_tests.js
+++ tests/user_unit_tests.js
@@ -3,36 +3,30 @@ require('dotenv').config();
 
 const test = require('ava').default;
 const User = require('../src/models/user');
-const db_connect = require('../src/config/mongoose.js');
 const {mongoose} = require('../src/config');
 
-async function createUser(username='',password='',email=''){
-    try {
-        const usr = await new User({username,password,email}).save();
-        return usr;
-    }
-    catch(e){
-        return Promise.reject(reason=e);
-    }
-}
+//test that user cant be crated without username ,password and email
 test('Create user without username/password/email',async t => {
     mongoose();
-    const result =  await t.throwsAsync(createUser());
-    t.is(result.message,'users validation failed: username: Username is required, password: User password is required, email: User email is required')
+    const result =  await t.throwsAsync(User.create({}));
+    t.is(result.message,'users validation failed: password: User password is required, username: Username is required, email: User email is required')
 });
 
+//test that user can't be created with password length shorter than 5 characters
 test('Create user with password less than minimum length',async t => {
     mongoose();
-    const result2 =  await t.throwsAsync(createUser('bot','beep','notabot@gmail.com'));
+    const result2 =  await t.throwsAsync(User.create({username:'bot',password:'beep',email:'notabot@gmail.com'}));
     t.is(result2.message,'users validation failed: password: Path `password` (`beep`) is shorter than the minimum allowed length (5).')
 });
 
+//test that a user can be created successfully with valid inputs
 test('Create user',async t => {
     mongoose();
     const result3 = await new User({username:'bot',password:'beepbop',email:'notabot@gmail.com'}).save(); 
     t.is(result3.username,'bot');
 });
 
+//test comparePassword function
 test('Compare password',async t => {
     mongoose();
     const result4 = await new User({username:'bot',password:'beepbop',email:'notabot@gmail.com'}).save(); 
